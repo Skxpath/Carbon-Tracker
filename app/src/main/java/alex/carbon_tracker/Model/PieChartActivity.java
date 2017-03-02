ac1d49d209ed6b1645a3dc789910efcf5e793abf
Sachin <sraturi>
2017-03-02 10:27:19 -0800

    Made an activity for the pieChart

---
+++ app/src/main/java/alex/carbon_tracker/Model/PieChartActivity.java
@@ -0,0 +1,15 @@
+package alex.carbon_tracker.Model;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import alex.carbon_tracker.R;
+
+public class PieChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pie_chart);
+    }
+}
