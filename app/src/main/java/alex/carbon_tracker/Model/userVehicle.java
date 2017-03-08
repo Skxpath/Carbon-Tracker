082d74321bd5a62a1914e58b89b973a6ef739a2f
Alex Zhang <cwzhang@sfu.ca>
2017-03-07 19:59:41 -0800

    A commit before merging into master

--- app/src/main/java/alex/carbon_tracker/Model/userVehicle.java
+++ app/src/main/java/alex/carbon_tracker/Model/userVehicle.java
@@ -10,14 +10,13 @@ import android.util.Log;
 public class UserVehicle extends Vehicle {
     private String nickname;
 
-    public UserVehicle() {
-    }
-
-    public UserVehicle(String make, String model, int year, String nickname) {
+    public UserVehicle(String make, String model, int year, String nickname, int cityDrive, int highwayDrive) {
         setMake(make);
         setModel(model);
         setYear(year);
         setNickname(nickname);
+        setCityDrive(cityDrive);
+        setHighwayDrive(highwayDrive);
     }
 
     public String getNickname() {
