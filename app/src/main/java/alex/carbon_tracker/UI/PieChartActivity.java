8e82356c7cb42bce7c130067903e3de6f0f37f4a
Sachin <sraturi>
2017-03-08 23:21:07 -0800

    Change one line testing

--- app/src/main/java/alex/carbon_tracker/UI/PieChartActivity.java
+++ app/src/main/java/alex/carbon_tracker/UI/PieChartActivity.java
@@ -43,7 +43,6 @@ public class PieChartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pie_chart);
 
         setJourneyData();
-
         setupPieChart();
         setupChangeButton();
     }
