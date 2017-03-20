c7f17fcad0108c9d44290ba17ef53f3bc2bec301
cwzhang <cwzhang@sfu.ca>
2017-03-20 15:04:19 -0700

    Revert "Merge branch 'TipManager-branch' into 'master'"
    This reverts merge request !40

--- app/src/main/java/alex/carbon_tracker/Model/CarbonTrackerModel.java
+++ app/src/main/java/alex/carbon_tracker/Model/CarbonTrackerModel.java
@@ -1,26 +1,49 @@
 package alex.carbon_tracker.Model;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+
+import alex.carbon_tracker.UI.WelcomeScreenActivity;
+
+import static android.content.Context.MODE_PRIVATE;
+
 /**
  * Carbon Tracker Model class which acts as a facade class for the
  * rest of the project. This class connects together JourneyManager,
  * RouteManager, VehicleManager, and UserVehicleManager.
  */
 public class CarbonTrackerModel {
-    private static CarbonTrackerModel ourInstance = new CarbonTrackerModel();
-
-    private static JourneyManager journeyManager;
-    private static RouteManager routeManager;
+    private static CarbonTrackerModel ourInstance;
+    private JourneyManager journeyManager;
+    private RouteManager routeManager;
     private static VehicleManager vehicleManager;
-    private static UserVehicleManager userVehicleManager;
-    private static TipManager tipManager;
-    private static TransportationManager transportationManager;
-
+    private UserVehicleManager userVehicleManager;
+    private TransportationManager transportationManager;
     public UserVehicleManager getUserVehicleManager() {
         return userVehicleManager;
     }
 
+    public static void getSavedModel(Context context) {
+        if (SaveData.getSharePreference(context) != null) {
+            ourInstance = SaveData.getSharePreference(context);
+        } else {
+            ourInstance = new CarbonTrackerModel();
+        }
+
+    }
+
     public static CarbonTrackerModel getInstance() {
         return ourInstance;
+    }
+
+    public TransportationManager getTransportationManager() {
+        return transportationManager;
     }
 
     private CarbonTrackerModel() {
@@ -28,8 +51,8 @@ public class CarbonTrackerModel {
         routeManager = new RouteManager();
         vehicleManager = new VehicleManager();
         userVehicleManager = new UserVehicleManager();
-        tipManager = new TipManager();
         transportationManager = new TransportationManager();
+        //userVehicleManager.testing();
     }
 
     public JourneyManager getJourneyManager() {
@@ -44,12 +67,5 @@ public class CarbonTrackerModel {
         return vehicleManager;
     }
 
-    public TipManager getTipManager() {
-        return tipManager;
-    }
 
-
-    public TransportationManager getTransportationManager() {
-        return transportationManager;
-    }
 }
--- app/src/main/java/alex/carbon_tracker/Model/TipManagerObserver.java
+++
@@ -1,9 +0,0 @@
-package alex.carbon_tracker.Model;
-
-/**
- * Created by Alex on 3/18/2017.
- */
-
-public interface TipManagerObserver {
-    void update();
-}
